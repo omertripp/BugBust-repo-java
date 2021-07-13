0f5c544012c5d4e5da2784e3c577ffa114076152
Vandita V <vandita.v@Vanditas-MacBook-Air.local>
2021-07-13 16:30:57 -0700

    Revert "Commit fix for Resource leak by bugbust player SecureScope"
    This reverts commit 2d2f7a59947a410134492b2533e463abd682466f.

--- bundles/com.amazonaws.eclipse.core/src/com/amazonaws/eclipse/core/ui/preferences/accounts/LegacyPreferenceStoreAccountMerger.java
+++ bundles/com.amazonaws.eclipse.core/src/com/amazonaws/eclipse/core/ui/preferences/accounts/LegacyPreferenceStoreAccountMerger.java
@@ -110,13 +110,10 @@ public class LegacyPreferenceStoreAccountMerger {
                                 "We cannot automatically merge the following accounts into your credentials file, " +
                                 "since the file already exists and is in invalid format. " +
                                 "Do you want to recreate the file using these account configurations?";
-                        try(MergeLegacyAccountsConfirmationDialog dialog = new MergeLegacyAccountsConfirmationDialog(
+                        MergeLegacyAccountsConfirmationDialog dialog = new MergeLegacyAccountsConfirmationDialog(
                                 null, MESSAGE, new String[] { "Yes", "No" }, 0,
-                                legacyAccounts.values(), null, null)) {
-                        	int result = dialog.open();
-			} catch (Exception exception) {
-				System.out.println(exception);
-			}
+                                legacyAccounts.values(), null, null);
+                        int result = dialog.open();
 
                         if (result == 0) {
                             AwsToolkitCore.getDefault().logInfo("Deleting the credentials file before dumping the legacy accounts.");
